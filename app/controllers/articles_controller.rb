c26ffb746a5fca122a2a6a554eaa0b79105ae78f
Andrew <andrewbrown.n2000@gmail.com>
2023-04-21 22:50:45 -0500

    added the create in CRUD functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  def index
+  def index # returns the index.html.erb file
     @articles = Article.all #gets all articles from the database
   end
 
@@ -8,4 +8,22 @@ class ArticlesController < ApplicationController
   end   # params[:id] is taken from the routes file
   # if the article ID is 1, then it would be articles/1
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"     # "articles#index" means the 'articles' controller,
                                 # but the 'index' action in that controller
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"  #:id is a parameter
+    #get "/articles", to: "articles#index"
+    #get "/articles/:id", to: "articles#show"  #:id is a parameter
+  resources :articles
 end
